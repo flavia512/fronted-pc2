7669f7a06dc69fff480cb789c4506c957c2c1fed
Flavia <flaviagonzales512@gmail.com>
2026-05-29 14:07:35 +0200

    ip de pc1 cambiada para presentacion

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api',
-  pc1Url: 'http://192.168.1.131:8001',
+  pc1Url: 'http://10.172.196.40:8001',
   mapboxToken: 'pk.eyJ1IjoiZmxhdmlhNTEyIiwiYSI6ImNtb2MzdnIzNDA3MHoyd3M2MmlwYnByOHcifQ.WlDllzzdq2XxMMIlCoWO2g'
 };
