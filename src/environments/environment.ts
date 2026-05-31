99c4eec00d7f0b0eb772b70a78f22b93661ffc08
Flavia <flaviagonzales512@gmail.com>
2026-05-31 13:43:31 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://IP_MV_LORCA:8000/api',
+  apiUrl: 'http://localhost:8000/api',
   pc1Url: 'http://10.172.196.40:8001',
   mapboxToken: 'pk.eyJ1IjoiZmxhdmlhNTEyIiwiYSI6ImNtb2MzdnIzNDA3MHoyd3M2MmlwYnByOHcifQ.WlDllzzdq2XxMMIlCoWO2g'
 };
