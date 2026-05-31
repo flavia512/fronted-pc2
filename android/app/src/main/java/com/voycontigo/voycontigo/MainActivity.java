0051db37a988f5a991bc651b8d5f6a492bab7579
IzanDLF <131258056+IzanDLF@users.noreply.github.com>
2026-05-31 15:00:39 +0200

    Capacitor

---
+++ android/app/src/main/java/com/voycontigo/voycontigo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.voycontigo.voycontigo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.voycontigo.voycontigo',
+  appName: 'VoyContigo',
+  webDir: 'dist/frontend-app/browser',
+};
+
+export default config;
